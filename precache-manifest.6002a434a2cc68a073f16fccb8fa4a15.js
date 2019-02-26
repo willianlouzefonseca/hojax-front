d6861e9a85c517210ec948fb1fddbca25e0b59d0
willian louze fonseca <williamlouzefonseca333@gmail.com>
2019-02-26 08:59:32 -0300

    production ready

---
+++ precache-manifest.6002a434a2cc68a073f16fccb8fa4a15.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "9924c2a6fb88d54e51f5",
+    "url": "/static/css/main.ff8edaac.chunk.css"
+  },
+  {
+    "revision": "9924c2a6fb88d54e51f5",
+    "url": "/static/js/main.9924c2a6.chunk.js"
+  },
+  {
+    "revision": "02f5cd560f4164e597af",
+    "url": "/static/css/1.c7a55186.chunk.css"
+  },
+  {
+    "revision": "02f5cd560f4164e597af",
+    "url": "/static/js/1.02f5cd56.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23c36b56e7a043cd16ed8273cf7fd52c",
+    "url": "/static/media/16.23c36b56.png"
+  },
+  {
+    "revision": "0eaa415aa0af12ef68d0f96defe33bfd",
+    "url": "/static/media/banner_bg.0eaa415a.png"
+  },
+  {
+    "revision": "ec1264653733425e6a1fa07e215de93c",
+    "url": "/static/media/pricingbg.ec126465.png"
+  },
+  {
+    "revision": "61a7bffaceaf858e25114120291f671c",
+    "url": "/static/media/24.61a7bffa.png"
+  },
+  {
+    "revision": "2027d00b33cbf8fe69ba27649a469eda",
+    "url": "/static/media/7.2027d00b.png"
+  },
+  {
+    "revision": "2b902aa7e877726e6a23a4c36f3e57d0",
+    "url": "/static/media/10.2b902aa7.png"
+  },
+  {
+    "revision": "40d6862ec90f08e797bd5430e844ead2",
+    "url": "/static/media/12.40d6862e.png"
+  },
+  {
+    "revision": "87bd3da17ff35c7f40d26b35bdfd1611",
+    "url": "/static/media/17.87bd3da1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "731d97f0940dc764ba1f0e135c1274dd",
+    "url": "/index.html"
+  }
+];
